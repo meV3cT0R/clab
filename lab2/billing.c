df443d85b850399d08176ff57ff2648573d00076
vector <vector@debian.vector>
2023-01-01 13:24:57 +0545

    readme

--- lab2/billing.c
+++ lab2/billing.c
@@ -17,8 +17,8 @@ void main() {
     if(unit <= 20) price =0;
     if(unit>20 && unit <=120) price += (unit-20)*12;
 
-    if(unit>120 && unit <= 220) price +=(unit-120)*11;
-    if(unit>220) price += (unit-220)*10;
+    if(unit>120 && unit <= 220) price +=(100*12)+(unit-120)*11;
+    if(unit>220) price += (100*12)+(100*11)+(unit-220)*10;
 
     printf("Billing Amount : %d \n",price);
 }
