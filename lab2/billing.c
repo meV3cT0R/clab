1e525274ee459fbc9d9f37e64717904746782a5e
vector <vector@debian.vector>
2023-01-01 13:31:53 +0545

    billing fix

--- lab2/billing.c
+++ lab2/billing.c
@@ -15,10 +15,10 @@ void main() {
     scanf("%d",&unit);
 
     if(unit <= 20) price =0;
-    if(unit>20 && unit <=120) price += (unit-20)*12;
+    if(unit>20 && unit <=120) price = (unit-20)*12;
 
-    if(unit>120 && unit <= 220) price +=(100*12)+(unit-120)*11;
-    if(unit>220) price += (100*12)+(100*11)+(unit-220)*10;
+    if(unit>120 && unit <= 220) price =(100*12)+(unit-120)*11;
+    if(unit>220) price = (100*12)+(100*11)+(unit-220)*10;
 
     printf("Billing Amount : %d \n",price);
 }
